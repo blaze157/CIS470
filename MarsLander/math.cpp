f542726ffbf7f4a4fa892d24b3d52d13e1a50194
Kyle Booth <eone157pl@gmail.com>
2018-08-15 11:46:38 -0700

    forgot to git add math

---
+++ MarsLander/math.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "math.h"
+
+double square(double num)
+{
+	return num*num;
+}
---
+++ MarsLander/math.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const double pi = 3.14159265;
+double square(double num);
